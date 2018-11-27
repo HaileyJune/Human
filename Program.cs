b56d932b9e303dbfc541a6f49d87ccdb07cb3af0
Hailey June <hailey.june.b@gmail.com>
2018-11-26 16:12:46 -0800

    added random and healing

--- Program.cs
+++ Program.cs
@@ -31,9 +31,25 @@ namespace Human
 
         public void Attack(Human thisOne)
         {
-            int hurt = 5 * strength;
+            // int hurt = 5 * strength;
+            Random rand = new Random();
+            int hurt = rand.Next(1,5) * strength;
             thisOne.health = thisOne.health - hurt;
             System.Console.WriteLine("{0} attacked {1} for {2} damage.", name, thisOne.name, hurt);
+        }
+
+        public void Heal()
+        {
+            Random rand = new Random();
+            int heal = rand.Next(1,5)*intellegence;
+            if (health + heal > 100){
+                health = 100;
+            }
+            else
+            {
+                health = health+heal;
+            }
+            System.Console.WriteLine("{0} healed themself and is now at {1} health.", name, health);
         }
     }
     class Program
@@ -43,6 +59,7 @@ namespace Human
             Human human1 = new Human("June");
             Human human2 = new Human("Jane");
             human1.Attack(human2);
+            human2.Heal();
             System.Console.WriteLine();
         }
     }
